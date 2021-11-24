d73a07e35f673e16df32ed9ac2e22747c18d9312
Lautaro Garavano <lgaravano28@gmail.com>
2021-11-24 12:44:27 -0300

    ah btw ya hice el 1b xddd

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -1,20 +1,22 @@
 module Eval1
-  ( eval
-  , Env
+  ( eval,
+    Env,
   )
 where
 
-import           AST
-import           Monads
-import qualified Data.Map.Strict               as M
-import           Data.Maybe
-import           Prelude                 hiding ( fst
-                                                , snd
-                                                )
-import           Data.Strict.Tuple
-import           Control.Monad                  ( liftM
-                                                , ap
-                                                )
+import AST
+import Control.Monad
+  ( ap,
+    liftM,
+  )
+import qualified Data.Map.Strict as M
+import Data.Maybe
+import Data.Strict.Tuple
+import Monads
+import Prelude hiding
+  ( fst,
+    snd,
+  )
 
 -- Entornos
 type Env = M.Map Variable Int
@@ -24,10 +26,10 @@ initEnv :: Env
 initEnv = M.empty
 
 -- Mónada estado
-newtype State a = State { runState :: Env -> Pair a Env }
+newtype State a = State {runState :: Env -> Pair a Env}
 
 instance Monad State where
-  return x = State (\s -> (x :!: s))
+  return x = State (x :!:)
   m >>= f = State (\s -> let (v :!: s') = runState m s in runState (f v) s')
 
 -- Para calmar al GHC
@@ -35,13 +37,14 @@ instance Functor State where
   fmap = liftM
 
 instance Applicative State where
-  pure  = return
+  pure = return
   (<*>) = ap
 
 instance MonadState State where
-  lookfor v = State (\s -> (lookfor' v s :!: s))
-    where lookfor' v s = fromJust $ M.lookup v s
-  update v i = State (\s -> (() :!: update' v i s)) where update' = M.insert
+  lookfor v = State (\s -> lookfor' v s :!: s)
+    where
+      lookfor' v s = fromJust $ M.lookup v s
+  update v i = State (\s -> () :!: update' v i s) where update' = M.insert
 
 -- Ejercicio 1.b: Implementar el evaluador utilizando la monada State
 
@@ -52,14 +55,45 @@ eval p = snd (runState (stepCommStar p) initEnv)
 -- Evalua multiples pasos de un comando, hasta alcanzar un Skip
 stepCommStar :: MonadState m => Comm -> m ()
 stepCommStar Skip = return ()
-stepCommStar c    = stepComm c >>= \c' -> stepCommStar c'
+stepCommStar c = stepComm c >>= \c' -> stepCommStar c'
 
 -- Evalua un paso de un comando
 stepComm :: MonadState m => Comm -> m Comm
-stepComm = undefined
+stepComm Skip = return Skip
+stepComm (Let s exp) = do
+  evalExp exp >>= update s
+  return Skip
+stepComm (Seq Skip co') = return co'
+stepComm (Seq co co') = do
+  r <- stepComm co
+  return $ Seq r co'
+stepComm (IfThenElse exp co co') = do
+  r <- evalExp exp
+  return $ if r then co else co'
+stepComm c@(While exp co) = do
+  r <- evalExp exp
+  return $ if r then Seq co c else Skip
 
 -- Evalua una expresion
 evalExp :: MonadState m => Exp a -> m a
-evalExp = undefined
-
-
+evalExp (Const n) = return n
+evalExp (Var s) = lookfor s
+evalExp (UMinus exp) = negate <$> evalExp exp
+evalExp (Plus exp exp') = (+) <$> evalExp exp <*> evalExp exp'
+evalExp (Minus exp exp') = (-) <$> evalExp exp <*> evalExp exp'
+evalExp (Times exp exp') = (*) <$> evalExp exp <*> evalExp exp'
+evalExp (Div exp exp') = div <$> evalExp exp <*> evalExp exp'
+evalExp BTrue = return True
+evalExp BFalse = return False
+evalExp (Lt exp exp') = (<) <$> evalExp exp <*> evalExp exp'
+evalExp (Gt exp exp') = (>) <$> evalExp exp <*> evalExp exp'
+evalExp (And exp exp') = (&&) <$> evalExp exp <*> evalExp exp'
+evalExp (Or exp exp') = (||) <$> evalExp exp <*> evalExp exp'
+evalExp (Not exp) = not <$> evalExp exp
+evalExp (Eq exp exp') = (==) <$> evalExp exp <*> evalExp exp'
+evalExp (NEq exp exp') = (/=) <$> evalExp exp <*> evalExp exp'
+evalExp (EAssgn s exp) = do
+  r <- evalExp exp
+  update s r
+  return r
+evalExp (ESeq exp exp') = evalExp exp >> evalExp exp'
